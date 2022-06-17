92f28fbf50170e1e2eac4b67d7277f5a0e1ec634
Moses Karani <karanim594@gmail.com>
2022-06-17 09:53:42 +0300

    created 3 models & registered them

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from hood.models import Business, Neighborhood, User
+
+
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Business)
+admin.site.register(User)
--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Neighborhood(models.Model):
+    neighborhood_name = models.CharField(max_length=100)
+    location = models.CharField()
+
+
+
+class User(models.Model):
+    name = models.CharField()
+    email = models.CharField()
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+
+
+class Business(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    business_name = models.CharField()
+    business_email = models.CharField()
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.save()
+        self.delete()
+
+    
