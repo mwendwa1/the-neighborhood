9ccbd85ab0b33209f00321519c2ad8b942ae3c8e
Moses Karani <karanim594@gmail.com>
2022-06-18 16:57:11 +0300

    created a view and template for all hoods

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,17 @@
+import re
 from django.shortcuts import render
+
+from hood.models import Neighborhood
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def hoods(request):
+    all_hoods = Neighborhood.objects.all()
+    all_hoods = all_hoods[::-1]
+    context = {'all_hoods':all_hoods}
+    return render(request, context, 'all_hoods.html')
+    
